395d5ff076820fffb522a302fa8ce9df673a25f7
jarondlk <jaronchai78@gmail.com>
2022-07-26 14:02:56 +0700

    add new file

---
+++ buttonNotPressed.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+pinNumLED = 5
+pinNumBTN = 16
+GPIO.setup(pinNumLED, GPIO.OUT)
+GPIO.setup(pinNumBTN, GPIO.IN)
+
+while True:
+
+       if (GPIO.input(pinNumBTN)):
+            GPIO.output(pinNumLED, GPIO.HIGH)
+
+        else:
+            GPIO.output(pinNumLED, GPIO.HIGH)
+            sleep(0.2)
+            GPIO.output(pinNumLED, GPIO.LOW)
+            sleep(0.2)
