db45047ef147d663e96eddaacf90f6e3db9ff333
jarondlk <jaronchai78@gmail.com>
2022-07-26 15:25:22 +0700

    update again lol

--- rgb_mod.py
+++ rgb_mod.py
@@ -43,13 +43,13 @@ def setup():
     # Set all led as pwm channel,
     #  and frequece to 2KHz
     p_R = GPIO.PWM(pins['Red'], 2000)
-    p_G = GPIO.PWM(pins['Green'], 2000)
-    p_B = GPIO.PWM(pins['Blue'], 2000)
+    # p_G = GPIO.PWM(pins['Green'], 2000)
+    # p_B = GPIO.PWM(pins['Blue'], 2000)
 
     # Set all begin with value 0
     p_R.start(0)
-    p_G.start(0)
-    p_B.start(0)
+    # p_G.start(0)
+    # p_B.start(0)
 
 # Define a MAP function for mapping values.
 # Like from 0~255 to 0~100
@@ -66,21 +66,24 @@ def MAP(x, in_min, in_max, out_min, out_max):
 def setColor(color):
     # Devide colors from 'color' veriable
     R_val = (color & 0xFF0000) >> 16
-    G_val = (color & 0x00FF00) >> 8
-    B_val = (color & 0x0000FF) >> 0
+    # G_val = (color & 0x00FF00) >> 8
+    # B_val = (color & 0x0000FF) >> 0
 
     # Map color value from 0~255 to 0~100
     R_val = MAP(R_val, 0, 255, 0, 100)
-    G_val = MAP(G_val, 0, 0, 0, 100)
-    B_val = MAP(B_val, 0, 0, 0, 100)
+    # G_val = MAP(G_val, 0, 0, 0, 100)
+    # B_val = MAP(B_val, 0, 0, 0, 100)
 
     # Change the colors
     p_R.ChangeDutyCycle(R_val)
-    p_G.ChangeDutyCycle(G_val)
-    p_B.ChangeDutyCycle(B_val)
+    # p_G.ChangeDutyCycle(G_val)
+    # p_B.ChangeDutyCycle(B_val)
 
-    print("color_msg: R_val = %s,	G_val = %s,	B_val = %s" %
-          (R_val, G_val, B_val))
+    # print("color_msg: R_val = %s,	G_val = %s,	B_val = %s" %
+    #       (R_val, G_val, B_val))
+
+    print("color_msg: R_val = %s " %
+          (R_val))
 
 
 def main():
@@ -94,8 +97,8 @@ def main():
 def destroy():
     # Stop all pwm channel
     p_R.stop()
-    p_G.stop()
-    p_B.stop()
+    # p_G.stop()
+    # p_B.stop()
     # Turn off all LEDs
     #GPIO.output(pins, GPIO.HIGH)
     # Release resource
